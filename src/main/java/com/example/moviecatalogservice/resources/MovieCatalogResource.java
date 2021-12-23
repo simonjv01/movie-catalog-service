eb5d93051637f7cc8ffd4c91d38a5dc3f6d2d6d4
Simon Vargas <sjvargas01@gmail.com>
2021-12-23 15:34:25 -0500

    added user rating, updated resource

--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,14 +3,13 @@ package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
-import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,13 +23,17 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
+//                Arrays.asList(
+//                new Rating("1234", 4),
+//                new Rating("5678", 3)
+
+
+        return ratings.getUserRating().stream().map(rating -> {
+                    // for each movie id, call movie info service and get details
            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/simon" + rating.getMovieId(), Movie.class);
+                    // put them all together
            return new CatalogItem(movie.getName(), "movie tEst desc", rating.getRating());
 
         })
@@ -38,8 +41,8 @@ public class MovieCatalogResource {
 
 
         //get all rated Movie IDs
-        // for each movie id, call movie info service and get details
-        // put them all together
+
+
 
 //        return Collections.singletonList(
 //                new CatalogItem("Transformers", "Test",4)
